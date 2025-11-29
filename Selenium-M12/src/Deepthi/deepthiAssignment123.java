45b71942a1d1b87bb668b9098fc8a5abcd461605
HP <HP@DESKTOP-ULE6OFS>
2025-11-29 11:39:54 +0530

    Deepthi

--- Selenium-M12/src/Deepthi/deepthiAssignment123.java
+++ Selenium-M12/src/Deepthi/deepthiAssignment123.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class deepthiAssignment {
+public class deepthiAssignment123 {
 	public static void main(String[] args) {
 		WebDriver driver =new ChromeDriver();
 		driver.manage().window().maximize();
