b495fcf62a18693b71de9c159e3d031fba9a1499
ANJANAYYA1 <115333771+ANJANAYYA1@users.noreply.github.com>
2025-11-29 11:44:55 +0530

    Prarathana commit

---
+++ Selenium-M12/src/Prarthana/PrarthanaDemo1.java
@@ -0,0 +1,5 @@
+package Prarthana;
+
+public class PrarthanaDemo1 {
+
+}
---
+++ Selenium-M12/src/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		ghp_20TictdVIdvnaDApYac4gn2LVXcMPv3MSDtJ
+	}
+}
+
